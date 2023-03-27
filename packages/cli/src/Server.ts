0930a00de3707afb1a16e888a1284b84b72fc303
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-03-27 23:22:16 +0200

    refactor and add unit tests for oauth controllers

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -148,6 +148,7 @@ import { getCurrentAuthenticationMethod } from './sso/ssoHelpers';
 import { isVersionControlLicensed } from '@/environments/versionControl/versionControlHelper';
 import { VersionControlService } from '@/environments/versionControl/versionControl.service.ee';
 import { VersionControlController } from '@/environments/versionControl/versionControl.controller.ee';
+import { CredentialsHelper } from '@/CredentialsHelper';
 
 const exec = promisify(callbackExec);
 
@@ -370,7 +371,7 @@ class Server extends AbstractServer {
 		return this.frontendSettings;
 	}
 
-	private registerControllers(ignoredEndpoints: Readonly<string[]>) {
+	private async registerControllers(ignoredEndpoints: Readonly<string[]>) {
 		const { app, externalHooks, activeWorkflowRunner, nodeTypes } = this;
 		const repositories = Db.collections;
 		setupAuthMiddlewares(app, ignoredEndpoints, this.restEndpoint, repositories.User);
@@ -382,13 +383,30 @@ class Server extends AbstractServer {
 		const samlService = Container.get(SamlService);
 		const versionControlService = Container.get(VersionControlService);
 
+		const encryptionKey = await UserSettings.getEncryptionKey();
+		const credentialsHelper = new CredentialsHelper(encryptionKey);
+
 		const controllers: object[] = [
 			new EventBusController(),
 			new AuthController({ config, internalHooks, repositories, logger, postHog }),
 			new OwnerController({ config, internalHooks, repositories, logger }),
 			new MeController({ externalHooks, internalHooks, repositories, logger }),
-			new OAuth1CredentialController(config, logger, externalHooks, repositories.Credentials),
-			new OAuth2CredentialController(config, logger, externalHooks, repositories.Credentials),
+			new OAuth1CredentialController(
+				config,
+				logger,
+				credentialsHelper,
+				externalHooks,
+				repositories.Credentials,
+				repositories.SharedCredentials,
+			),
+			new OAuth2CredentialController(
+				config,
+				logger,
+				credentialsHelper,
+				externalHooks,
+				repositories.Credentials,
+				repositories.SharedCredentials,
+			),
 			new NodeTypesController({ config, nodeTypes }),
 			new PasswordResetController({
 				config,
@@ -511,7 +529,7 @@ class Server extends AbstractServer {
 
 		await handleLdapInit();
 
-		this.registerControllers(ignoredEndpoints);
+		await this.registerControllers(ignoredEndpoints);
 
 		this.app.use(`/${this.restEndpoint}/credentials`, credentialsController);
 
